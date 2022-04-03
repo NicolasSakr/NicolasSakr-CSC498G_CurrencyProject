d862d2a69a485eb9fb76a0657e197f61ee9f7319
anthonyykh <anthonykh01.ak@gmail.com>
2022-04-03 20:48:05 +0300

    split the get method into get lbp and get dollar

---
+++ get_lbp.php
@@ -0,0 +1,22 @@
+<?php
+
+include("db_info.php");
+
+$id = $_GET["id"]; 
+
+$query = $mysqli->prepare("SELECT lbp FROM dollarrates WHERE id = ?");
+$query->bind_param("i", $id);
+$query->execute();
+
+$array = $query->get_result();
+
+$response = [];
+
+while($course = $array->fetch_assoc()){
+    $response[] = $course;
+}
+
+$json_response = json_encode($response);
+echo $json_response;
+
+?>
